e771d19495a78fcb63c2f51126ca89fadc5b5013
Jeffrey Pfau <jeffrey@endrift.com>
2016-06-25 13:01:54 -0700

    Code golf!

--- src/bios-dump.c
+++ src/bios-dump.c
@@ -33,11 +33,10 @@ int main() {
 		"push {r4-r10,lr} \n"
 		"bx r12 \n"
 		"mov r0, #0xE000000 \n"
-		"orr r3, r0, #0x4000 \n"
 		"0: \n"
 		"ldrb r2, [r11], #1 \n"
 		"strb r2, [r0], #1 \n"
-		"cmp r0, r3 \n"
+		"cmp r11, r10 \n"
 		"blt 0b \n"
 	: : : "r0", "r1", "r2", "r3", "r11", "r12", "memory");
 	printf("Done dumping!");
